ad5eb648900603738464761284e2128c56644ae2
Ashutosh Prajapati <ashutoshkumarcse@gmail.com>
2016-05-18 17:12:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+
+
+Template.login.events({
+  'click button'(event, instance) {
+
+ var x=document.getElementById('u').value;
+	var y=  document.getElementById('p').value;
+	  if((x=="cdac")&&(y=="cdac"))
+	  {
+			alert("User authenticated");
+		  Meteor.Router.to('/home');
+	  }
+	  else
+	  {
+		  alert("Not authenticated");
+	  }
+	 
+	 var msg =  document.getElementById("cmm").value;
+     document.getElementById('d').innerHTML = msg;
+	  
+  },
+});
